68ddcde8d073e379be97a18352e4123df3b51443
Johannes Braun <johannes@maniac-labs.com>
2015-11-10 12:12:11 +0100

    add travis and example code

--- demo/src/main/java/com/lovoo/tutorialbubbledemo/MainActivity.java
+++ demo/src/main/java/com/lovoo/tutorialbubbledemo/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.lovoo.tutorialbubbles.TutorialScreen;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TutorialScreen buttonTutorialScreen;
 
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
@@ -26,6 +32,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final Button explainButton = (Button) findViewById(R.id.explain_button);
+        explainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick ( View v ) {
+                buttonTutorialScreen.showTutorial();
+            }
+        });
+
+        explainButton.post(new Runnable() {
+            @Override
+            public void run () {
+                buttonTutorialScreen = new TutorialScreen.TutorialBuilder(R.layout.button_tutorial_layout, explainButton)
+                        .setParentLayout(findViewById(R.id.root_layout))
+                        .setDismissible(true)
+                        .addHighlightView(explainButton, true)
+                        .setOnTutorialLayoutInflatedListener(new TutorialScreen.OnTutorialLayoutInflatedListener() {
+                            @Override
+                            public void onLayoutInflated ( View view ) {
+                                // put code here for tutorial
+                                view.findViewById(R.id.tutorial_inner_button).setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick ( View v ) {
+                                        Toast.makeText(MainActivity.this, "Some buton action", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        })
+                        .build();
+
+            }
+        });
+
     }
 
     @Override
